03272bc4084d0d3b74ad36164a36bcbceb27a334
putra4648 <wakwaujaim@gmail.com>
2025-05-27 12:55:34 +0700

    feat: added frontend pages

--- src/main/java/id/putra/wms/controller/PageController.java
+++ src/main/java/id/putra/wms/controller/PageController.java
@@ -11,12 +11,44 @@ public class PageController {
     @GetMapping("/")
     public String index(@AuthenticationPrincipal OidcUser user, Model model) {
         model.addAttribute("name", user.getUserInfo().getGivenName());
+        model.addAttribute("title", "Dashboard");
         return "pages/index";
     }
 
-    @GetMapping("/dashboard")
-    public String dashboard() {
-        return "pages/dashboard";
+    @GetMapping("/logistic")
+    public String logistic(Model model) {
+        model.addAttribute("title", "Logistic");
+        return "pages/logistic";
     }
 
+    @GetMapping("/order")
+    public String order(Model model) {
+        model.addAttribute("title", "Order");
+        return "pages/order";
+    }
+
+    @GetMapping("/inventory")
+    public String inventory(Model model) {
+        model.addAttribute("title", "Inventory");
+        return "pages/inventory";
+    }
+
+
+    @GetMapping("/report_and_analysis")
+    public String reportAndAnalysis(Model model) {
+        model.addAttribute("title", "Report & Analysis");
+        return "pages/report_and_analysis";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("title", "User");
+        return "pages/user";
+    }
+
+    @GetMapping("/setting")
+    public String setting(Model model) {
+        model.addAttribute("title", "Setting");
+        return "pages/setting";
+    }
 }
