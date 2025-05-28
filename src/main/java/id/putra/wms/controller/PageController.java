f7f66635a248eb32528bbab4fe5b7be4c8f5182e
putra4648 <wakwaujaim@gmail.com>
2025-05-28 13:20:25 +0700

    feat: added sample user

--- src/main/java/id/putra/wms/controller/PageController.java
+++ src/main/java/id/putra/wms/controller/PageController.java
@@ -10,50 +10,50 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
     @GetMapping("/")
     public String index(@AuthenticationPrincipal OidcUser user, Model model) {
-        model.addAttribute("name", user.getUserInfo().getGivenName());
-        model.addAttribute("title", "Dashboard");
+        user.getFullName();
+
         return "pages/index";
     }
 
     @GetMapping("/outbound")
     public String logistic(Model model) {
-        model.addAttribute("title", "Outbound");
+
         return "pages/outbound";
     }
 
     @GetMapping("/inbound")
     public String order(Model model) {
-        model.addAttribute("title", "Inbound");
+
         return "pages/inbound";
     }
 
     @GetMapping("/inventory")
     public String inventory(Model model) {
-        model.addAttribute("title", "Inventory");
+
         return "pages/inventory";
     }
 
     @GetMapping("/supplier")
     public String supplier(Model model) {
-        model.addAttribute("title", "Supplier");
+
         return "pages/supplier";
     }
 
     @GetMapping("/report_and_analysis")
     public String reportAndAnalysis(Model model) {
-        model.addAttribute("title", "Report & Analysis");
+
         return "pages/report_and_analysis";
     }
 
     @GetMapping("/user")
     public String user(Model model) {
-        model.addAttribute("title", "User");
+
         return "pages/user";
     }
 
     @GetMapping("/setting")
     public String setting(Model model) {
-        model.addAttribute("title", "Setting");
+
         return "pages/setting";
     }
 }
