1b763d1072a19d43374e683627547eb08c21ed38
putra4648 <wakwaujaim@gmail.com>
2025-06-11 13:45:20 +0700

    chore: fix WebMvcConfig trailing slash

--- src/main/java/id/putra/wms/config/WebMvcConfig.java
+++ src/main/java/id/putra/wms/config/WebMvcConfig.java
@@ -10,6 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/public", "classpath:/static/");
+                .addResourceLocations("/public/", "classpath:/static/");
     }
 }
