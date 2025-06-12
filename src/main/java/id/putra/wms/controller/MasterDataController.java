f123494f121a5609d7ecb6631bba0d606530fca8
putra4648 <wakwaujaim@gmail.com>
2025-06-12 23:10:49 +0700

    fix: removed product detail API

--- src/main/java/id/putra/wms/controller/MasterDataController.java
+++ src/main/java/id/putra/wms/controller/MasterDataController.java
@@ -1,7 +1,5 @@
 package id.putra.wms.controller;
 
-import java.util.Optional;
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,7 +8,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,20 +28,9 @@ public class MasterDataController implements ErrorController {
 
   private final ProductService productService;
 
-  @GetMapping("master/product/{id}")
-  public String getProductDetail(@PathVariable Optional<String> id, Model model) {
-    if (id.isPresent()) {
-      model.addAttribute("productForm", productService.getProductById(id.get()));
-      model.addAttribute("showModal", true);
-    }
-    return "pages/master/product";
-  }
-
   @GetMapping("master/product")
   public String getProduct(Model model) {
     model.addAttribute("productForm", new ProductDto());
-    // model.addAttribute("deleteProductForm", new ProductDto());
-
     return "pages/master/product";
   }
 
