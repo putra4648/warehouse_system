95d2b29c5756cea73fd158ceb1a289a624eb6b3f
putra4648 <wakwaujaim@gmail.com>
2025-08-18 19:32:08 +0700

    refactor: rework frontend

---
+++ web/app/components/Sidebar.vue
@@ -0,0 +1,108 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+
+
+const items = ref<NavigationMenuItem[][]>([
+    [
+        {
+            label: 'WMS PRO',
+            type: 'label'
+        },
+        {
+            label: 'Dashboard',
+            href: '/dashboard',
+            icon: 'i-heroicons-chart-bar-20-solid',
+        },
+        {
+            label: 'Inventory',
+
+            icon: 'i-heroicons-cube-20-solid',
+        },
+        {
+            label: "Inbound",
+
+            icon: 'i-heroicons-arrow-down-tray-20-solid',
+        },
+        {
+            label: "Outbound",
+
+            icon: 'i-heroicons-arrow-up-tray-20-solid',
+        },
+        // {
+        //     label: "Supplier",
+        //   
+        //     icon: 'i-heroicons-truck-20-solid'
+        // },
+        {
+            label: 'Reports & Analytics',
+
+            icon: 'i-heroicons-chart-pie-20-solid',
+        },
+        {
+            label: "Master Data",
+
+            icon: 'i-heroicons-circle-stack-20-solid',
+            children: [
+                {
+                    label: 'Product',
+
+                    icon: 'i-heroicons-users-20-solid',
+                },
+                {
+                    label: 'Warehouse',
+
+                    icon: 'i-heroicons-shield-check-20-solid',
+                },
+                {
+                    label: 'Supplier',
+
+                    icon: 'i-heroicons-truck-20-solid',
+                },
+            ]
+        },
+        {
+            label: 'Outbound',
+
+            icon: 'i-heroicons-arrow-up-tray-20-solid',
+        },
+        {
+            label: 'Warehouse',
+
+            icon: 'i-heroicons-building-office-20-solid',
+        },
+    ],
+    [
+        {
+            label: 'Settings',
+
+            icon: 'i-heroicons-cog-20-solid',
+        },
+        {
+            label: 'Help & Support',
+
+            icon: 'i-heroicons-question-mark-circle-20-solid',
+            children: [
+                {
+                    label: 'Documentation',
+
+                    icon: 'i-heroicons-book-open-20-solid',
+                },
+                {
+                    label: 'Contact Support',
+
+                    icon: 'i-heroicons-envelope-20-solid',
+                },
+            ]
+        },
+        {
+            label: 'Logout',
+
+            icon: 'i-heroicons-arrow-right-on-rectangle-20-solid',
+        },
+    ]
+])
+</script>
+<template>
+    <UNavigationMenu orientation="vertical" :items="items" class="data-[orientation=vertical]:w-48" />
+
+</template>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/scripts', '@nuxt/icon']
-})
+  modules: ["@nuxt/eslint", "@nuxt/scripts", "@nuxt/icon", "@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "WMS PRO",
+    },
+  },
+});
