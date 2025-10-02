10c0a7ce1cec36d2081abf7644ea534e86d89e3e
Putra <wakwaujaim@gmail.com>
2025-10-02 01:49:39 +0000

    feat: add security module

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,34 +2,16 @@ import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
-	modules: [
-		"@nuxt/eslint",
-		"@nuxt/scripts",
-		"nuxt-security",
-		(_options, nuxt) => {
-			nuxt.hooks.hook("vite:extendConfig", (config) => {
-				config.plugins?.push(vuetify({ autoImport: true }));
-			});
-		},
-	],
-	pages: true,
-	css: ["~/assets/app-tabulator.scss"],
-	app: {
-		head: {
-			title: "WMS PRO",
-		},
-	},
-	security: {},
-	build: {
-		transpile: ["vuetify"],
-	},
-	vite: {
-		vue: {
-			template: {
-				transformAssetUrls,
-			},
-		},
-	},
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/scripts", "@nuxt/icon", "@nuxt/ui", "nuxt-security"],
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "WMS PRO",
+    },
+  },
+  security: {
+    strict: true,
+  }
 });
