86f14014b64406e614ab814915f4fe4e745cd8b7
Putra <wakwaujaim@gmail.com>
2025-10-09 06:45:47 +0000

    feat: add new master pages

---
+++ web/app/pages/master/product.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <v-row class="mb-4">
+            <v-col>
+                <h1>Products</h1>
+            </v-col>
+            <v-col class="d-flex justify-end">
+                <v-btn color="primary" @click="add">
+                    <v-icon left>mdi-plus</v-icon>
+                    Add Product
+                </v-btn>
+            </v-col>
+        </v-row>
+        <div ref="myTable" />
+        <v-btn ref="myButton" @click="save">Save</v-btn>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import useTabulator from "../../composables/useTabulator";
+
+
+const myTable = ref<HTMLElement | null>(null);
+
+const { tabulatorInstance } = useTabulator(myTable, [
+    {
+        title: "Name",
+        field: "title",
+        headerFilter: "input",
+        editor: "input",
+    },
+    {
+        title: "Location",
+        field: "location",
+        headerFilter: "input",
+        editor: "input",
+    },
+    {
+        title: "Capacity",
+        field: "capacity",
+        hozAlign: "right",
+        formatter: "money",
+        formatterParams: {
+            decimal: ",",
+            thousand: ".",
+            symbol: "kg",
+            symbolAfter: true,
+            precision: 0,
+        },
+        headerFilter: "input",
+    },
+    {
+        headerSort: false,
+        title: "Actions",
+        field: "actions",
+        hozAlign: "center",
+        formatter: function () {
+            return "<i class='mdi mdi-trash-can'></i>";
+        },
+        cellClick: function (e, cell) {
+            //delete row on click
+            cell.getRow().delete();
+        },
+    },
+]);
+
+const save = () => {
+    console.log(tabulatorInstance.value?.getData());
+};
+
+const add = () => {
+    tabulatorInstance.value?.addRow({});
+};
+
+onMounted(() => {
+});
+</script>
