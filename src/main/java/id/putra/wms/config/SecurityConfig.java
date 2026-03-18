cecb3c37ea0abe7451fce051ec34d683375d2245
putra4648 <wakwaujaim@gmail.com>
2026-03-18 20:46:41 +0700

    fix: update security config via `@EnabledWebSecurity`

--- src/main/java/id/putra/wms/config/SecurityConfig.java
+++ src/main/java/id/putra/wms/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
