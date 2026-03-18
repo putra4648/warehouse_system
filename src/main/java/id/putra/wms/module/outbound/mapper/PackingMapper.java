edb84cf6774661b5934675bd24c6dbfd41902fa6
putra4648 <wakwaujaim@gmail.com>
2026-03-18 21:26:35 +0700

    fix: mapper dto

--- src/main/java/id/putra/wms/module/outbound/mapper/PackingMapper.java
+++ src/main/java/id/putra/wms/module/outbound/mapper/PackingMapper.java
@@ -1,21 +1,38 @@
 package id.putra.wms.module.outbound.mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 import id.putra.wms.module.outbound.dto.PackingDto;
 import id.putra.wms.module.outbound.model.entity.Packing;
+import id.putra.wms.module.outbound.model.entity.Shipment;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PackingMapper {
-    @Mapping(target = "shipments", ignore = true)
+    @Mapping(source = "shipmentIds", target = "shipments", qualifiedByName = "mapIdsToShipments")
     Packing toEntity(PackingDto dto);
 
-    @Mapping(target = "shipmentIds", ignore = true)
+    @Mapping(source = "shipments", target = "shipmentIds", qualifiedByName = "mapShipmentsToIds")
     PackingDto toDto(Packing entity);
 
     List<PackingDto> toDtos(List<Packing> entities);
+
+    @Named("mapShipmentsToIds")
+    default List<Long> mapShipmentsToIds(List<Shipment> shipments) {
+        return shipments.stream().map(Shipment::getId).collect(Collectors.toList());
+    }
+
+    @Named("mapIdsToShipments")
+    default List<Shipment> mapIdsToShipments(List<Long> shipmentIds) {
+        return shipmentIds.stream().map(id -> {
+            Shipment shipment = new Shipment();
+            shipment.setId(id);
+            return shipment;
+        }).collect(Collectors.toList());
+    }
 }
